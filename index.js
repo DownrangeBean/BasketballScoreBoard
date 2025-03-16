59e67532a79f1ec168820a38fb557606fa6b4eca
James Stammers <James.h.stammers@gmail.com>
2025-03-16 15:49:43 +0100

    Initial commit
    Changes to be committed:
    	new file:   fonts/CursedTimerUlil-Aznm.ttf
    	new file:   fonts/Seven Segment.ttf
    	new file:   index.css
    	new file:   index.html
    	new file:   index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+var scorecards = document.querySelectorAll('.score-card')
+
+scorecards.forEach(element => {
+    let buttonsArr = Array.from(element.getElementsByClassName('button-increment'));
+    let scoreElement = element.getElementsByClassName('score').item(0);
+    
+    if (scoreElement == null) {
+        console.warn('Cannot locate a score element!')
+    }
+
+    buttonsArr.forEach((element, idx) => {
+        element.textContent = '+' + (idx + 1);
+        element.addEventListener('click', () => {
+            clickHandler(idx + 1, scoreElement);
+        })
+    });
+
+});
+
+
+function clickHandler(amount, scoreEl) {
+    if (scoreEl == null) {
+        console.warn('score element is null');
+        return
+    }
+
+    let currentScore = Number(scoreEl.textContent);
+    scoreEl.textContent = currentScore + amount;
+    
+}
